69842ba299cd4a6aa4feea3e830ef4f6688bcafd
Wojciech Szymański <idlealist@gmail.com>
2024-02-20 17:01:43 +0100

    updated authMiddleware

--- Middlewares/authMiddleware.js
+++ Middlewares/authMiddleware.js
@@ -1,8 +1,10 @@
 function isAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
+        if (req.params.username && req.params.username !== req.user.username) {
+            return res.status(403).send('Access denied');
+        }
         return next();
     }
-
     res.redirect('/login');
 }
 
